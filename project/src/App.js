cb69666cf4f674dfb549906d7e8a8e9577c404a3
911carpediem <carpediem9011@gmail.com>
2026-04-01 12:15:05 +0900

    todo

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import TodoInput from './TodoInput'; //라헬 담당
+import TodoList from './TodoList'; //굥 담당
 
-function App() {
+  function App() {
+  // 1. 전체 할 일 목록 상태 (이게 B님한테 전달됨)
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Redux 공부하기", isDone: false },
+    { id: 2, text: "리액트 협업하기", isDone: true }
+  ]);
+
+  // 2. [체크박스 토글] 함수 (B님이 클릭하면 실행됨)
+  const onToggle = (id) => {
+    setTodos(todos.map(todo => 
+      todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+    ));
+  };
+
+  // 3. [삭제] 함수 (B님이 클릭하면 실행됨)
+  const onRemove = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  // 4. [추가] 함수 (A님이 입력하면 실행됨)
+  const onCreate = (text) => {
+    const newTodo = { id: Date.now(), text, isDone: false };
+    setTodos([...todos, newTodo]);
+  };
+
   return (
-    <div className="App">
+    <div style={{ width: '400px', margin: '0 auto', padding: '20px' }}>
+      <h1>Todo List</h1>
+      {/* A님 파트 */}
+      <TodoInput onCreate={onCreate} /> 
       
+      {/* B님 파트 (데이터와 함수들을 전달!) */}
+      <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
     </div>
   );
 }
